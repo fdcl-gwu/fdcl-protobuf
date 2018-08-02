226368d02a226f1879511679b5f739ebab375b5e
Shankar Kulumani <shanks.k@gmail.com>
2018-08-02 14:24:32 -0400

    Add build travis script

---
+++ script/build_travis.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+EIGEN_VER=3.3.4
+INSTALL_DIR="/usr/local/include"
+EIGEN_RELEASE_URL="https://github.com/eigenteam/eigen-git-mirror/archive/${EIGEN_VER}.tar.gz"
+TEMP_DIR="$(mktemp -d)"
+WORK_DIR="$(pwd)"
+
+PROTOBUF_VER=3.6.1
+INSTALL_DIR="/usr/local/protobuf"
+
+# download  latest release of eigen
+if [[ ! "$TEMP_DIR" || ! -d "$TEMP_DIR" ]]; then
+	echo "Could not create temp dir"
+	exit 1
+fi
+
+# delete the temp directory on cleanup
+function cleanup {
+    rm -rf "$TEMP_DIR"
+    echo "Deleted temp working directory $TEMP_DIR"
+}
+
+# trap cleanup EXIT
+
+echo "We're going to download Eigen ${EIGEN_VER} and install to ${INSTALL_DIR}"
+cd ${TEMP_DIR}
+mkdir ${EIGEN_VER}
+wget ${EIGEN_RELEASE_URL} -O ${TEMP_DIR}/${EIGEN_VER}.tar.gz
+tar -xvzf ${EIGEN_VER}.tar.gz -C ./${EIGEN_VER} --strip-components=1
+
+echo "Going to install Eigen using CMake"
+cd ${EIGEN_VER}
+mkdir build
+cd build
+cmake ..
+sudo make install
+
+echo "Eigen installed"
+
+# This will download the latest eigen and install for the sy tem
+echo "Going to install v${PROTOBUF_VER} Google Protocol Buffers"
+
+echo "We're going to download the repo to ${INSTALL_DIR}"
+cd ${TEMP_DIR}
+git clone ${PROTOBUF_URL} 
+
+echo "Going to install using the configure script"
+cd protobuf
+git submodule update --init --recursive
+./autogen.sh
+
+./configure --prefix=${INSTALL_DIR}
+make
+make check
+sudo make install
+
+echo "Finished Protobuf install"
